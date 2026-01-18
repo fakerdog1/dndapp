1d113bbeb745856be14e1637c722028e328f242f
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-18 16:22:31 +0200

    test changes

--- app/Models/CharacterClass.php
+++ app/Models/CharacterClass.php
@@ -8,7 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasMany
 
 /**
  * Class CharacterClass
@@ -35,7 +35,7 @@ class CharacterClass extends Model
 {
     protected $table = 'classes';
 
-    protected $fillable = [
+    protected $fillable = [;
         'name',
         'slug',
         'description',
