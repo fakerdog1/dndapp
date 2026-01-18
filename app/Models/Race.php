ab8ebe9d547819977a3b8884703f98e28e18f1e5
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-18 17:55:18 +0200

    test

--- app/Models/Race.php
+++ app/Models/Race.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Models\Misc\SizeCategory;
 use App\Models\Misc\SourceBook;
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder
 use Illuminate\Database\Eloquent\Model
 use Illuminate\Database\Eloquent\Relations\BelongsTo
 
