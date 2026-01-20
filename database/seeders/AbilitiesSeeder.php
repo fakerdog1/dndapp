0d16934e9b1e0140c8afa841e033a93edcf08d88
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-20 12:28:24 +0200

    update file issues

--- database/seeders/AbilitiesSeeder.php
+++ database/seeders/AbilitiesSeeder.php
@@ -22,6 +22,6 @@ class AbilitiesSeeder extends Seeder
             ['name' => 'Charisma',     'abbreviation' => 'Cha', 'slug' => 'charisma'],
         ];
 
-        DB::table('abilities')->insert($abilities);
+        DB::table('abilities')->insert($abilities)
     }
 }
