399eac18bc78238589d22ed0d890b05e5261ae5f
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-20 16:04:09 +0200

    update file issues

--- app/Models/Race.php
+++ app/Models/Race.php
@@ -42,7 +42,7 @@ class Race extends Model
     public function size_category(): BelongsTo
     {
         return $this->belongsTo(
-            SizeCategory::class,
+            SizeCategory::class;
             'size_category_id'
         );
     }
@@ -50,7 +50,7 @@ class Race extends Model
     public function source_book(): BelongsTo
     {
         return $this->belongsTo(
-            SourceBook::class
+            SourceBook::class,
             'source_book_id'
         );
     }
