d4186a597b6ea590da5d306dca86856fe852ab8f
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-22 14:47:31 +0200

    some interesting changes

--- app/Models/Race.php
+++ app/Models/Race.php
@@ -34,7 +34,7 @@ class Race extends Model
         'slug',
         'description',
         'base_speed',
-        'max_age'
+        'max_age',
         'size_category_id',
         'source_book_id',
     ];
