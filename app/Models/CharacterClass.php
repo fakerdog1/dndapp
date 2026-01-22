c573b3b18d93d6529840fdb516e520e8cd896ab5
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-22 09:34:14 +0200

    introduce changes

--- app/Models/CharacterClass.php
+++ app/Models/CharacterClass.php
@@ -8,7 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Class CharacterClass
