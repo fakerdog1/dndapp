da1fe01c0a38e4a2a464f816f9534450ca850e2d
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-22 11:31:44 +0200

    some interesting changes

--- app/Models/CharacterClass.php
+++ app/Models/CharacterClass.php
@@ -8,7 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasMany
 
 /**
  * Class CharacterClass
@@ -58,7 +58,7 @@ class CharacterClass extends Model
         );
     }
 
-    public function source_book(): BelongsTo
+    public function source_book(): Belongs
     {
         return $this->belongsTo(
             SourceBook::class,
