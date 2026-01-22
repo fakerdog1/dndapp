3d10d784ca6d46d51bb88da6330a9f2f801af13d
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-22 10:42:58 +0200

    some interesting changes

--- app/Models/Race.php
+++ app/Models/Race.php
@@ -47,7 +47,7 @@ class Race extends Model
         );
     }
 
-    public function source_book(): Belongs
+    public function source_book(): BelongsTo
     {
         return $this->belongsTo(
             SourceBook::class,
