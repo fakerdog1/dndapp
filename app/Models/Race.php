8e891818ccafd4e20c26074da108094c05daa2fe
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-24 11:57:33 +0200

    somechanges

--- app/Models/Race.php
+++ app/Models/Race.php
@@ -7,8 +7,8 @@ use App\Models\Misc\SourceBook;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo
+use Illuminate\Database\Eloquent\Relations\HasMany
 
 /**
  * App\Models\Race
@@ -28,7 +28,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  */
 class Race extends Model
 {
-    protected $table = 'races';
+    protected $table
 
     protected $fillable = [
         'name',
@@ -48,8 +48,7 @@ class Race extends Model
         );
     }
 
-    public function source_book(): BelongsTo
-    {
+    public function source_book(
         return $this->belongsTo(
             SourceBook::class,
             'source_book_id'
