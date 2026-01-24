551259538d3850c69d8e29f8b961aaf29b482329
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-24 11:59:43 +0200

    somechanges

--- app/Models/CharacterClass.php
+++ app/Models/CharacterClass.php
@@ -7,8 +7,8 @@ use App\Models\Misc\SourceBook;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo
-use Illuminate\Database\Eloquent\Relations\HasMany
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Class CharacterClass
@@ -40,7 +40,7 @@ class CharacterClass extends Model
         'slug',
         'description',
         'can_prepare_spells',
-        'hit_die_id'
+        'hit_die_id',
         'source_book_id'
     ];
 
@@ -58,10 +58,10 @@ class CharacterClass extends Model
         );
     }
 
-    public function source_book(): Belongs
+    public function source_book(): BelongsTo
     {
         return $this->belongsTo(
-            SourceeBook::class,
+            SourceBook::class,
             'source_book_id'
         );
     }
