90b8eea2c2af3f1a8ba2ec35bfdad94f0086fa08
Martin Simeonov <simeonoff.martin@gmail.com>
2026-01-26 17:04:21 +0200

    fixing fixing

--- app/Models/CharacterClass.php
+++ app/Models/CharacterClass.php
@@ -7,8 +7,8 @@ use App\Models\Misc\SourceBook;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo
-use Illuminate\Database\Eloquent\Relations\HasMany
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Class CharacterClass
@@ -33,18 +33,18 @@ use Illuminate\Database\Eloquent\Relations\HasMany
  */
 class CharacterClass extends Model
 {
-    protected $table = 'classes'
+    protected $table = 'classes';
 
     protected $fillable = [
         'name',
         'slug',
         'description',
         'can_prepare_spells',
-        'hit_die_id'
+        'hit_die_id',
         'source_book_id'
     ];
 
-    public function subclasses(): Has
+    public function subclasses(): HasMany
     {
         return $this->hasMany(Subclass::class, 'class_id');
     }
@@ -58,7 +58,7 @@ class CharacterClass extends Model
         );
     }
 
-    public function source_book
+    public function source_book()
         return $this->belongsTo(
             SourceBook::class,
             'source_book_id'
